1bea5bdfa0090a9bc026bd1fcc538f003d7ad968
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2024-06-24 00:53:42 +0530

    create user

--- arc_authenticate/actions/register.ts
+++ arc_authenticate/actions/register.ts
@@ -3,12 +3,34 @@
 import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
 
+import { db } from "@/lib/db";
+import bcrypt from 'bcrypt'
+import { getUserByEmail } from "@/data/user";
+
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = await RegisterSchema.safeParse(values)
-    if(!validatedFields) {
+    if(!validatedFields.success) {
         return {error: "Invalid fields!"}
     }
 
-    return { success: "Email Sent!"}
+    const {email, password, name} = validatedFields.data;
+
+    const userExists = await getUserByEmail(email)
+
+    if(userExists) {
+        return {error: "Email already in use."}
+    }
+
+    
+    const hashedPassword = await bcrypt.hash(password, 10)
+    
+    await db.user.create({
+        data: {
+            name, 
+            email, 
+            password: hashedPassword
+        }
+    })
+    return { success: "User Created!"}
 }
